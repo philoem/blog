c47cae54b6493d089c6e2b6be6cfe88943eef306
philoem <philoem@nexgate.ch>
2018-04-03 18:17:42 +0200

    .gitignore

--- public/index.php
+++ public/index.php
@@ -5,12 +5,10 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-		<link href="../public/style.css" rel="stylesheet" type="text/css" >
-		<title>Blog J.Forteroche</title>
-	</head>
+	
+<!-- Ici le header  -->
+		<?php include ('../views/inc/head_html.php'); ?>
+
 	<body>
 		<div class="container-fluid">
 <!-- Ici le header  -->
@@ -20,5 +18,6 @@ session_start();
 			<?php include ('../controlers/index_dispatch.php'); ?>
 
 		</div>
+		
 	</body>
 </html>
---
+++ views/inc/head_html.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Blog J.Forteroche</title>
+    <meta name="description" content="Roman de Jean Forteroche">
+    <!--POUR LA VUE SUR LES SMARTPHONES, LA LARGEUR DE L'ECRAN=LARGEUR ECRAN | L'UTILISATEUR NE PEUT PAS ZOOMER SUR L'ECRAN -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!-- PROTOCOLE OPEN GRAPH POUR ETRE VU DES RESEAUX SOCIAUX(FB, ETC) --> 
+    <meta property="og:title" content="Billet simple pour l'Alaska" >
+    <meta property="og:description" content="Ecriture de billets sur l'Alaska" >
+    <meta property="og:type" content="website" >
+    <!--AFFICHE LA PETITE ICONE SUR TOUS LES NAVIAGTEURS DONT IE -->
+    <link rel="shortcut icon" type="image/x-icon" href="Photos/favicon.ico" >
+    <link rel="icon" type="image/png" href="photos/favicon.ico" >
+    <!--AFFICHE LA PETITE ICONE SUR TOUS LES NAVIAGTEURS DONT IE -->		
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="../public/style.css" rel="stylesheet" type="text/css" >
+</head>
