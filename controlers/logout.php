469dcee9acb1e48ba3132cc573144fe452f74cc5
philoem <philoem@nexgate.ch>
2018-04-19 22:06:26 +0200

    logout avec SESSION & COOKIE

--- controlers/logout.php
+++ controlers/logout.php
@@ -3,8 +3,8 @@ session_start();
 session_destroy();
 
 // Ici destruction des cookies existants
-setcookie('pseudo', '', time() -3600);
-setcookie('password', '', time() -3600);	
+setcookie('pseudo', '', time() -3600, '/', null, false, true);
+setcookie('password', '', time() -3600, '/', null, false, true);	
 
 header('location:../index.php');
 
