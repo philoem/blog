bff0d848e50f235ce0697dcda6bf5c1663bff34d
philoem <philoem@nexgate.ch>
2018-04-25 18:24:06 +0200

    reglages bugs responsivite

--- views/inc/header_index.php
+++ views/inc/header_index.php
@@ -1,8 +1,9 @@
 <header>
 	<nav class="navbar fixed-top navbar-dark bg-dark">
 		<div class="navbar-brand" id="content_header">
-            <h1 class="font-weight-normal">"Billet simple pour l'Alaska"</h1>
-            <a href="../controlers/user_postControl.php" id="paragraph_header" title="Cliquez ici pour lire le roman en entier" class="font-weight-light"><em>écrit par l'auteur Jean Forteroche</em></a>
+			
+			<h1 class="font-weight-normal">"Billet simple pour l'Alaska"</h1>
+			<a href="../controlers/user_postControl.php" id="paragraph_header" title="Cliquez ici pour lire le roman en entier" class="font-weight-light"><em>écrit par l'auteur Jean Forteroche</em></a>
 			
 		</div>
 	</nav>
--- views/user_post.php
+++ views/user_post.php
@@ -7,17 +7,18 @@
 		<div class="container">
 <!-- Ici le header --> 
 			<?php include '../views/inc/header_user_post.php'; ?>
-
-			<?php 
-			
-			foreach($bookManager->readAll() as $donnees):
-				if ($donnees['approuved'] == 1) {
-					echo'<p>'.htmlspecialchars($donnees['billet']).'</p>';
-					
-				}
-			endforeach;
+			<div class ="col-xs-12 col-lg-12" id="readingBillets">
+				<?php 
 				
-			?>
+				foreach($bookManager->readAll() as $donnees):
+					if ($donnees['approuved'] == 1) {
+						echo'<p>'.htmlspecialchars($donnees['billet']).'</p>';
+						
+					}
+				endforeach;
+					
+				?>
+			</div>
 						
 			
 			
