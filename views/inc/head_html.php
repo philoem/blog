a60fa08fd252944281d572217b9b5880f117cac6
philoem <philoem@nexgate.ch>
2018-04-30 10:05:49 +0200

    modif head

--- views/inc/head_html.php
+++ views/inc/head_html.php
@@ -1,17 +1,19 @@
-<meta charset="UTF-8">
+
+    <meta charset="UTF-8">
 
 
-<meta name="description" content="Roman de Jean Forteroche">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Roman de Jean Forteroche">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<meta property="og:title" content="Billet simple pour l'Alaska" >
-<meta property="og:description" content="Ecriture de billets sur l'Alaska" >
-<meta property="og:type" content="website" >
+    <meta property="og:title" content="Billet simple pour l'Alaska" >
+    <meta property="og:description" content="Ecriture de billets sur l'Alaska" >
+    <meta property="og:type" content="website" >
 
-<link rel="shortcut icon" type="image/x-icon" href="../public/img/alaska.ico" >
-<link rel="icon" type="image/png" href="../public/img/alaska.ico" >
+    <link rel="shortcut icon" type="image/x-icon" href="../public/img/alaska.ico" >
+    <link rel="icon" type="image/png" href="../public/img/alaska.ico" >
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link href="../public/style.css" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="../public/style.css" rel="stylesheet" type="text/css" >
 
-<title>Blog J.Forteroche</title>
+    <title>Blog J.Forteroche</title>
+
