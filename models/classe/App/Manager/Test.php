a98e34eed22fe1d8984b945f3284903d62ddd547
philoem <philoem@nexgate.ch>
2018-04-30 13:15:56 +0200

    test 2

---
+++ models/classe/App/Manager/Test.php
@@ -0,0 +1,15 @@
+<?php
+namespace classe\App\Manager;
+
+use \PDO;
+use classe\App\Entity\Book;
+
+/**
+ * classe BookManager héritée de la classe DbConnect
+ * 
+ */
+
+class BookManager extends DbConnect {
+
+
+}
