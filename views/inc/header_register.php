b1b2f7c5a42c846dfbf5938bcada541ee374dc21
philoem <philoem@nexgate.ch>
2018-04-30 17:07:22 +0200

    correction  style register.php

--- views/inc/header_register.php
+++ views/inc/header_register.php
@@ -3,7 +3,7 @@ session_start();
 // Sécurisation de la page pour empêcher l'accès via une réécriture de l'url
 if (!isset($_SESSION['pseudo']) AND !isset($_SESSION['password'])) {
 
-    header('Location: ../views/login.php');
+    header('Location: ../controlers/AccesControl.php');
 
 } elseif (isset($_COOKIES['pseudo']) AND isset($_COOKIES['password'])) {
 	
@@ -15,8 +15,8 @@ if (!isset($_SESSION['pseudo']) AND !isset($_SESSION['password'])) {
 	<nav class="navbar fixed-top navbar-dark bg-primary">
 		<div class="navbar-brand" id="content_header_register">
 			<h1 class="font-weight-normal">Inscription</h1>
-			<a href="../index.php" id="paragraph_header" title="Cliquez ici pour retourner à la page accueil"><p id="link_register_index">Retour à la page accueil</p></a>
-			<a href="../controlers/user_postControl.php" id="paragraph_header" title="Cliquez ici pour lire le roman en entier" class="font-weight-light"><p><em>"Billet simple pour l'Alaska"</em></p></a>
+			<a href="../index.php" class="paragraph_header" title="Cliquez ici pour retourner à la page accueil"><p id="link_register_index">Retour à la page accueil</p></a>
+			<a href="../controlers/user_postControl.php" class="paragraph_header" title="Cliquez ici pour lire le roman en entier" ><p class="font-weight-light"><em>"Billet simple pour l'Alaska"</em></p></a>
 		</div>
 	</nav>
 </header>
